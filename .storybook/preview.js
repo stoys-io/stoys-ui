bf863f94bbcfcfb980b41a36de73e294f0720a58
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-08-09 16:24:51 +0300

    feat(ci): add docgen

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+const { addDecorator } = require('@storybook/react');
+const { withPropsTable } = require('storybook-addon-react-docgen');
+
+addDecorator(withPropsTable());
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 }
