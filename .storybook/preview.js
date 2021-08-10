72f1e72515d360ae128f33aa3e2fbaf7ebe4e83c
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-08-10 16:00:55 +0300

    style: fix code styling

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
-const { addDecorator } = require('@storybook/react');
-const { withPropsTable } = require('storybook-addon-react-docgen');
+const { addDecorator } = require('@storybook/react')
+const { withPropsTable } = require('storybook-addon-react-docgen')
 
-addDecorator(withPropsTable());
+addDecorator(withPropsTable())
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
--- src/joinRates/styles.ts
+++ src/joinRates/styles.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 import Table from 'antd/lib/table'
 
-export const StyledJoinStatistics = styled(Table)<{smallSize: boolean}>`
+export const StyledJoinStatistics = styled(Table)<{ smallSize: boolean }>`
   margin-bottom: 20px;
 
   .ant-table-tbody {
@@ -15,10 +15,10 @@ export const StyledJoinStatistics = styled(Table)<{smallSize: boolean}>`
   }
 
   td.ant-table-cell {
-    padding: ${(props) => props.smallSize ? '2px' : '16px'};
+    padding: ${props => (props.smallSize ? '2px' : '16px')};
   }
 
   th.ant-table-cell {
-    padding: ${(props) => props.smallSize ? '4px' : '16px'};
+    padding: ${props => (props.smallSize ? '4px' : '16px')};
   }
 `
