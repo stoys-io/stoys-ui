c2df00283fb242c7187bc603874ff0f4f0a48f16
Andrew Ratnikov <ratnikov.am@gmail.com>
2021-08-10 12:45:48 +0300

    feat(ci): add docgen (#19)
    Co-authored-by: Andrew Ratnikov <andrii.ratnikov@agileengile.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,9 @@
-const path = require('path')
-
 module.exports = {
   core: {
     builder: 'webpack5',
   },
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-docs', 'storybook-addon-react-docgen'],
   typescript: {
     check: true,
     checkOptions: { async: false },
