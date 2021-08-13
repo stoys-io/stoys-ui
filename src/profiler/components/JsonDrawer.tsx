8529298e78d11882ec7713727e82701b9be9244a
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-08-13 13:42:26 +0300

    feat(profiler): remove json viewer; use string view instead

--- src/profiler/components/JsonDrawer.tsx
+++ src/profiler/components/JsonDrawer.tsx
@@ -1,30 +1,13 @@
-import React, { ChangeEvent, useCallback, useState } from 'react'
+import React from 'react'
 import Drawer from 'antd/lib/drawer'
-import Input from 'antd/lib/input'
-import debounce from 'lodash.debounce'
-import ReactJson from 'searchable-react-json-view'
 
 import 'antd/lib/drawer/style/css'
 
 import { JsonDrqwerProps } from '../model'
 
-const { Search } = Input
-
 const JsonDrawer = ({ visible, onClose, datasets }: JsonDrqwerProps): JSX.Element => {
-  const [search, setSearch] = useState<string>()
-
-  const _onSearch = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      event.persist()
-
-      debounce(() => setSearch(event.target.value), 300)()
-    },
-    [setSearch]
-  )
-
   return (
     <Drawer
-      title={<Search placeholder="Search" allowClear onSearch={setSearch} onChange={_onSearch} />}
       placement="right"
       closable={false}
       onClose={onClose}
@@ -34,13 +17,7 @@ const JsonDrawer = ({ visible, onClose, datasets }: JsonDrqwerProps): JSX.Elemen
       style={{ position: 'absolute' }}
       destroyOnClose
     >
-      <ReactJson
-        src={datasets}
-        iconStyle="square"
-        displayObjectSize={false}
-        displayDataTypes={false}
-        highlightSearch={search}
-      />
+      <pre>{JSON.stringify(datasets, null, 2)}</pre>
     </Drawer>
   )
 }
