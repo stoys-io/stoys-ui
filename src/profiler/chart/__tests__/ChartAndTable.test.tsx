397502a7b707070143c447905ae593ac4e959ef3
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-09-02 12:16:32 +0300

    test(profiler): update tests

--- src/profiler/chart/__tests__/ChartAndTable.test.tsx
+++ src/profiler/chart/__tests__/ChartAndTable.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import '../../../__mocks__/matchMedia.mock'
 
-import Chart from '../Chart'
+import Chart from '../ChartAndTable'
 import { CheckedRowsContext } from '../../checkedRowsContext'
 
 beforeAll(() => {
