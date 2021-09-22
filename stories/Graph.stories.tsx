95eb92128efaca798f731f4d9f4623dc84e3cc46
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-09-22 19:30:36 +0300

    feat(graph): add data

--- stories/Graph.stories.tsx
+++ stories/Graph.stories.tsx
@@ -3,6 +3,8 @@ import { Story } from '@storybook/react'
 import { Graph as GraphComponent, GraphProps } from '../src'
 
 import dataMock from './mocks/graph/dag_inlined.json'
+import difDataMock1 from './mocks/graph/20210922_120415_152372/dag_inlined.json'
+import difDataMock2 from './mocks/graph/20210922_125856_444212/dag_inlined.json'
 import { nodes } from './mocks/graph/Nodes.mock'
 import { edges } from './mocks/graph/Edges.mock'
 import { combos } from './mocks/graph/Combos.mock'
@@ -21,9 +23,15 @@ BigGraph.args = {
   nodes, edges, combos
 }
 
+export const DifGraph = Template.bind({})
+DifGraph.storyName = 'Diffing'
+DifGraph.args = {
+  data: difDataMock1 as any
+}
+
 export default {
   title: 'Chart/Graph',
-  component: [Graph, BigGraph],
+  component: [Graph, BigGraph, DifGraph],
   parameters: {
     layout: 'fullscreen',
   }
