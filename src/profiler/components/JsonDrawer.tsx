c4b6008f917aa0cfa83724c29cce17115e9b4c58
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-09-22 15:31:32 +0300

    feat(profiler): add json search

--- src/profiler/components/JsonDrawer.tsx
+++ src/profiler/components/JsonDrawer.tsx
@@ -1,33 +1,57 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useCallback, ChangeEvent, useState } from 'react'
 import Drawer from 'antd/lib/drawer'
+import Input from 'antd/lib/input'
 import { BigJsonViewerDom } from 'big-json-viewer'
+import debounce from 'lodash.debounce'
 
 import 'antd/lib/drawer/style/css'
 import 'big-json-viewer/styles/default.css'
 
+import { SearchWrapper } from './styles'
+
 import { JsonDrqwerProps } from '../model'
+
+const { Search } = Input
 
 const JsonDrawer = ({ visible, onClose, datasets }: JsonDrqwerProps): JSX.Element => {
   const jsonViewerRef = useRef<any>(null)
+  const [searchValue, onSearch] = useState<string>('')
+
+  const _onSearchChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      event.persist()
+
+      debounce(() => onSearch(event.target.value), 300)()
+    },
+    [onSearch]
+  )
 
   useEffect(() => {
     const initJsonViewer = async () => {
-      const viewer = await BigJsonViewerDom.fromObject(datasets, {
-        linkLabelCopyPath: undefined,
-        linkLabelExpandAll: undefined,
-      })
-      const node = viewer.getRootElement()
-      jsonViewerRef.current.appendChild(node)
-      await node.openAll()
+      try {
+        const viewer = await BigJsonViewerDom.fromObject(datasets, {
+          linkLabelCopyPath: undefined,
+        })
+        const node = viewer.getRootElement()
+        jsonViewerRef.current.appendChild(node)
 
-      if (!visible) {
-        viewer.destroy()
+        if (searchValue) {
+          await viewer.openBySearch(new RegExp(searchValue, 'i'))
+        }
+      } catch (err) {
+        console.error(err)
       }
     }
 
-    // tslint:disable-next-line: no-floating-promises
-    initJsonViewer()
-  }, [datasets, visible])
+    if (visible) {
+      // tslint:disable-next-line: no-floating-promises
+      initJsonViewer()
+    }
+
+    return () => {
+      jsonViewerRef.current.innerHTML = ''
+    }
+  }, [datasets, visible, searchValue])
 
   return (
     <Drawer
@@ -40,6 +64,9 @@ const JsonDrawer = ({ visible, onClose, datasets }: JsonDrqwerProps): JSX.Elemen
       style={{ position: 'absolute' }}
       destroyOnClose
     >
+      <SearchWrapper>
+        <Search placeholder="Search" allowClear onSearch={onSearch} onChange={_onSearchChange} />
+      </SearchWrapper>
       <div ref={jsonViewerRef} />
       {/* <pre>{JSON.stringify(datasets, null, 2)}</pre> */}
     </Drawer>
---
+++ src/profiler/components/styles.ts
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled'
+
+export const SearchWrapper = styled.div`
+  margin-bottom: 5px;
+`
