f28bceeab751bf68eee54820d045b3e3e25dfd79
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-09-28 16:08:09 +0300

    feat(graph): sidebar prev run

--- stories/Graph.stories.tsx
+++ stories/Graph.stories.tsx
@@ -3,6 +3,8 @@ import { Story } from '@storybook/react'
 import { Graph as GraphComponent } from '../src'
 
 import dataMock from './mocks/graph/dag_inlined.json'
+import diffDataMock1 from './mocks/graph/20210922_120415_152372/dag_inlined.json'
+import diffDataMock2 from './mocks/graph/20210922_125856_444212/dag_inlined.json'
 import { nodes } from './mocks/graph/Nodes.mock'
 import { edges } from './mocks/graph/Edges.mock'
 import { combos } from './mocks/graph/Combos.mock'
@@ -17,6 +19,12 @@ Graph2.args = {
   chromaticScale: 'interpolatePuOr',
 }
 
+export const DiffGraph = Template.bind({})
+DiffGraph.storyName = 'with diffing'
+DiffGraph.args = {
+  data: [diffDataMock1, diffDataMock2] as any,
+}
+
 export const BigGraph = Template.bind({})
 BigGraph.storyName = 'Big data'
 BigGraph.args = {
@@ -28,7 +36,7 @@ BigGraph.args = {
 
 export default {
   title: 'Chart/Graph',
-  component: [Graph2, BigGraph],
+  component: [Graph2, BigGraph, DiffGraph],
   parameters: {
     layout: 'fullscreen',
   },
