775fc1295d1050bfbe848c0c3d21232ea4bd0c99
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-09-30 13:22:20 +0300

    feat(graph): second click on column name disable filtering

--- src/graph/Graph.tsx
+++ src/graph/Graph.tsx
@@ -40,39 +40,60 @@ const Graph2 = ({ data, config /* , chromaticScale */ }: Props) => {
   }
   const drawerData = tables?.find(table => table.id === drawerNodeId)
 
-  const setColumnHighlight = (columnId: string, tableId: string) => {
-    const selectedTableSourceIds = graph.edges
-      .filter(edge => edge.target === tableId)
-      ?.map(edge => edge.source)
-    const selectedTableTargetsIds = graph.edges
-      .filter(edge => edge.source === tableId)
-      ?.map(edge => edge.target)
-    const sourceTableColumnIds = tables
-      ?.filter(table => selectedTableSourceIds.includes(table.id))
-      .map(table => table.columns.find(column => column.dependencies?.includes(columnId))?.id)
-    const selectedColumnDependcies = tables
-      ?.find(table => table.id === tableId)
-      ?.columns.find(column => column.id === columnId)?.dependencies
-    const columnDependcies = [
-      ...(sourceTableColumnIds ? sourceTableColumnIds : []),
-      ...(selectedColumnDependcies ? selectedColumnDependcies : []),
-    ].filter(notEmpty)
-    const tableIds = [...selectedTableSourceIds, ...selectedTableTargetsIds].filter(notEmpty)
+  const [_columnId, _setColumnId] = useState<string>('')
 
-    setGraph({
-      ...graph,
-      nodes: graph.nodes.map(node => ({
-        ...node,
-        data: {
-          ...node.data,
-          highlightedColumns: {
-            selectedTableId: tableId,
-            selectedColumnId: columnId,
-            reletedColumnsIds: columnDependcies,
-            reletedTablesIds: tableIds,
+  const setColumnHighlight = (columnId: string, tableId: string) => {
+    _setColumnId(prevState => {
+      if (columnId === prevState) {
+        setGraph({
+          ...graph,
+          nodes: graph.nodes.map(node => ({
+            ...node,
+            data: {
+              ...node.data,
+              highlightedColumns: undefined,
+            },
+          })),
+        })
+
+        return ''
+      }
+
+      const selectedTableSourceIds = graph.edges
+        .filter(edge => edge.target === tableId)
+        ?.map(edge => edge.source)
+      const selectedTableTargetsIds = graph.edges
+        .filter(edge => edge.source === tableId)
+        ?.map(edge => edge.target)
+      const sourceTableColumnIds = tables
+        ?.filter(table => selectedTableSourceIds.includes(table.id))
+        .map(table => table.columns.find(column => column.dependencies?.includes(columnId))?.id)
+      const selectedColumnDependcies = tables
+        ?.find(table => table.id === tableId)
+        ?.columns.find(column => column.id === columnId)?.dependencies
+      const columnDependcies = [
+        ...(sourceTableColumnIds ? sourceTableColumnIds : []),
+        ...(selectedColumnDependcies ? selectedColumnDependcies : []),
+      ].filter(notEmpty)
+      const tableIds = [...selectedTableSourceIds, ...selectedTableTargetsIds].filter(notEmpty)
+
+      setGraph({
+        ...graph,
+        nodes: graph.nodes.map(node => ({
+          ...node,
+          data: {
+            ...node.data,
+            highlightedColumns: {
+              selectedTableId: tableId,
+              selectedColumnId: columnId,
+              reletedColumnsIds: columnDependcies,
+              reletedTablesIds: tableIds,
+            },
           },
-        },
-      })),
+        })),
+      })
+
+      return columnId
     })
   }
 
