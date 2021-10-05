f79b02713d3db03cc3d6fd8ec09a32cfad60b6f9
Dmytro Getmantsov <dmytro.getmantsov@agileengine.com>
2021-10-05 14:41:52 +0300

    feat(graph): remove unloadable data

--- stories/Graph.stories.tsx
+++ stories/Graph.stories.tsx
@@ -7,7 +7,6 @@ import diffDataMock1 from './mocks/graph/20210922_120415_152372/dag_inlined.json
 import diffDataMock2 from './mocks/graph/20210922_125856_444212/dag_inlined.json'
 
 import grpcData from './mocks/graph/graph.bazel/grpc/dag.json'
-/* import tensorboardData from './mocks/graph/graph.bazel/tensorboard/dag.json' */
 
 const Template: Story<any> = args => <GraphComponent {...args} />
 
@@ -30,13 +29,6 @@ BigGraph.storyName = 'lots of data - grpc'
 BigGraph.args = {
   data: grpcData,
 }
-
-/* export const BigGraph2 = Template.bind({})
- * BigGraph.storyName = 'lots of data - tensorboard'
- * BigGraph.args = {
- *   data: tensorboardData,
- * }
- *  */
 
 export default {
   title: 'Chart/Graph',
