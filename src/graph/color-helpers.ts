c589ed887ef260ba8bf967ce4b9bff0592255269
Dmytro Getmantsov <dmytro.getmantsov@agileengine.com>
2021-10-12 13:00:59 +0300

    feat(graph): update color highlighting

--- src/graph/color-helpers.ts
+++ src/graph/color-helpers.ts
@@ -3,22 +3,12 @@ import { Highlight, ChromaticScale } from './model'
 
 export const fromColorPallete =
   (maxRank: number, highlight: Highlight, chromaticScale: ChromaticScale) => (rank: number) => {
-    let gradient
-    if (chromaticScale === 'interpolateInferno') {
-      gradient =
-        highlight === 'nearest'
-          ? [0.25, 0.75]
-          : highlight === 'children'
-          ? getFlatGradient(0.5, 1, maxRank)
-          : getFlatGradient(0, 0.5, maxRank).reverse()
-    } else {
-      gradient =
-        highlight === 'nearest'
-          ? getGradientNearest()
-          : highlight === 'children'
-          ? getFlatGradient(0.75, 1, maxRank)
-          : getFlatGradient(0, 0.25, maxRank).reverse()
-    }
+    const gradient =
+      highlight === 'nearest'
+        ? getGradientNearest()
+        : highlight === 'children'
+        ? getFlatGradient(0.75, 1, maxRank)
+        : getFlatGradient(0, 0.25, maxRank).reverse()
 
     return getChromaticColor(gradient[rank - 1], chromaticScale)
   }
