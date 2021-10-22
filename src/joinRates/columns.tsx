ff490a7ca43f9e4f2f7c9dcb2244a54fe0b3f4fe
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-10-22 19:21:53 +0300

    chore(join-rates): table names column width; tooltip; three dots

--- src/joinRates/columns.tsx
+++ src/joinRates/columns.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Tooltip from 'antd/lib/tooltip'
 
 import { renderNumericCell } from '../common'
+import { TableHeaderCellText } from './styles'
 
 export const getJoinRatesColumns = (columns: Array<string>) =>
   columns.map(column => {
@@ -9,9 +10,18 @@ export const getJoinRatesColumns = (columns: Array<string>) =>
       id: column,
       title: () => <Tooltip title={column}>{column}</Tooltip>,
       dataIndex: column,
+      ...(column === 'Table names' ? { width: `${(2 / columns.length) * 100}%` } : {}),
       render: (data: Array<string> | number) => {
         if (data && Array.isArray(data) && column === 'Table names') {
-          return data.join(', ')
+          return (
+            <>
+              {data.map(dataItem => (
+                <Tooltip key={dataItem} title={dataItem}>
+                  <TableHeaderCellText>{dataItem}</TableHeaderCellText>
+                </Tooltip>
+              ))}
+            </>
+          )
         }
 
         if (data && typeof data === 'number') {
