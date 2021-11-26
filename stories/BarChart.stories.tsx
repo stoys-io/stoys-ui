b79bb9e722d32d897d8049759f7cb561ac89f4b1
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-11-26 12:13:38 +0200

    feat(bar-chart): add mock data

--- stories/BarChart.stories.tsx
+++ stories/BarChart.stories.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Story } from '@storybook/react'
 
 import BarChartComponent from '../src/BarChart'
-// import { BarChartData, BarChartThreeDatasets, BarChartPassengerCount } from './mocks/BarChart.mock'
+import { BarChartData, BarChartThreeDatasets } from './mocks/BarChart.mock'
 
 const Template: Story<any> = args => <BarChartComponent {...args} />
 
 export const BarChart = Template.bind({})
 BarChart.args = {
-  dataset: [],
+  dataset: [BarChartData],
   config: {
     height: 500,
   },
@@ -17,7 +17,7 @@ BarChart.storyName = 'One dataset'
 
 export const BarChartWithFewDatasets = Template.bind({})
 BarChartWithFewDatasets.args = {
-  dataset: [],
+  dataset: BarChartThreeDatasets,
   config: {
     dataType: 'integer',
     height: 500,
@@ -25,18 +25,7 @@ BarChartWithFewDatasets.args = {
 }
 BarChartWithFewDatasets.storyName = 'Three datasets'
 
-export const BarChartWithThreeDatasets = Template.bind({})
-BarChartWithThreeDatasets.args = {
-  dataset: [],
-  config: {
-    height: 500,
-    showAxes: false,
-    showLogScale: false,
-  },
-}
-BarChartWithThreeDatasets.storyName = 'Three datasets *'
-
 export default {
   title: 'Chart/BarChart',
-  component: [BarChart, BarChartWithThreeDatasets, BarChartWithFewDatasets],
+  component: [BarChart, BarChartWithFewDatasets],
 }
---
+++ stories/mocks/BarChart.mock.ts
@@ -0,0 +1,93 @@
+export const BarChartData = [
+  {
+    count: 100,
+    item: 'a'
+  },
+  {
+    count: 20,
+    item: 'b'
+  },
+  {
+    count: 50,
+    item: 'c'
+  },
+  {
+    count: 80,
+    item: 'd'
+  },
+]
+
+export const BarChartThreeDatasets = [
+  [
+    {
+      count: 100,
+      item: 'a'
+    },
+    {
+      count: 20,
+      item: 'b'
+    },
+    {
+      count: 50,
+      item: 'c'
+    },
+    {
+      count: 80,
+      item: 'd'
+    },
+  ],
+  [
+    {
+      count: 10,
+      item: 'a'
+    },
+    {
+      count: 120,
+      item: 'b'
+    },
+    {
+      count: 30,
+      item: 'c'
+    },
+    {
+      count: 50,
+      item: 'd'
+    },
+  ],
+  [
+    {
+      count: 30,
+      item: 'a'
+    },
+    {
+      count: 70,
+      item: 'b'
+    },
+    {
+      count: 10,
+      item: 'c'
+    },
+    {
+      count: 0,
+      item: 'd'
+    },
+  ],
+  [
+    {
+      count: 45,
+      item: 'a'
+    },
+    {
+      count: 95,
+      item: 'b'
+    },
+    {
+      count: 20,
+      item: 'c'
+    },
+    {
+      count: 5,
+      item: 'd'
+    },
+  ]
+]
