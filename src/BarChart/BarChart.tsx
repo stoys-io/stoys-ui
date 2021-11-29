b8338581f5a13df32c4371f524ed0a935202de4c
Andrew Ratnikov <andrii.ratnikov@agileengile.com>
2021-11-29 15:29:16 +0200

    feat(bar-chart): add tooltip

--- src/BarChart/BarChart.tsx
+++ src/BarChart/BarChart.tsx
@@ -29,6 +29,9 @@ const BarChart = ({ dataset, config }: BarChartProps): JSX.Element => {
 
   const ref = useD3(
     (svg: any) => {
+      const windowWidth = window.innerWidth
+      const windowHeight = window.innerHeight
+
       const { width } = svg.node().getBoundingClientRect()
 
       const yType = showLogScale ? d3.scaleLog : d3.scaleLinear
@@ -46,6 +49,13 @@ const BarChart = ({ dataset, config }: BarChartProps): JSX.Element => {
 
       const xScale = d3.scaleBand(xDomain, xRange).padding(0.025)
       const yScale = yType(yDomain, yRange)
+
+      const body = d3.select('body')
+      const tooltip = body.append('div').style('pointer-events', 'none')
+
+      svg
+        .on('pointerenter pointermove', pointermoved)
+        .on('pointerleave', () => tooltip.style('display', 'none').selectChildren().remove())
 
       if (showAxes) {
         const xAxis = d3
@@ -93,7 +103,7 @@ const BarChart = ({ dataset, config }: BarChartProps): JSX.Element => {
 
       const _height = showLogScale ? yScale(1) : yScale(0)
 
-      const bar = svg
+      svg
         .append('g')
         .selectAll('rect')
         .data(I)
@@ -103,6 +113,61 @@ const BarChart = ({ dataset, config }: BarChartProps): JSX.Element => {
         .attr('height', (i: any) => _height - yScale(Y[i]))
         .attr('width', xScale.bandwidth())
         .attr('fill', (i: any) => chartData[i].color)
+
+      function pointermoved(event: any) {
+        const [xCoordinate, yCoordinate] = d3.pointer(event) // [x, y]
+        const scale = d3.scaleQuantize(xDomain).domain(xRange)
+        const xValue = scale(xCoordinate)
+
+        const tooltipValues = chartData.filter((value: any) => value.item === xValue)
+
+        const colorBadge = (color: string) => `
+            <span style="
+              display: inline-block;
+              width: 10px;
+              height: 10px;
+              border-radius: 100%;
+              margin-right: 5px;
+              margin-left: 5px;
+              background-color: ${color};
+            "></span>
+          `
+
+        if (tooltipValues.length) {
+          tooltip
+            .style('display', 'block')
+            .attr('class', 'plot-tooltip')
+            .style('padding', '2px')
+            .style('position', 'fixed')
+            .style('background-color', '#fff')
+            .style('border', '1px solid #000')
+            .html(() => {
+              return tooltipValues
+                .map(
+                  value =>
+                    `<div>${colorBadge(value.color)} ${value.item} <b>${value.count}</b></div>`
+                )
+                .join('')
+            })
+
+          const { width: tooltipWidth, height: tooltipHeight } = tooltip
+            .node()!
+            .getBoundingClientRect()
+
+          let y = event.y + 15
+          let x = event.x + 15
+
+          if (x + tooltipWidth > windowWidth) {
+            x = x - tooltipWidth - 15
+          }
+
+          if (y + tooltipHeight > windowHeight) {
+            y = y - tooltipHeight - 15
+          }
+
+          tooltip.style('top', `${y}px`).style('left', `${x}px`)
+        }
+      }
     },
     [config]
   )
